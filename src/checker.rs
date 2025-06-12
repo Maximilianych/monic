b66c2fce195b086a565f36c06a7aab856fd53f0a
Maximilianych <maxgorochov27@gmail.com>
2025-06-12 20:40:52 +0300

    Рабочая проверка

--- src/checker.rs
+++ src/checker.rs
@@ -1,3 +1,13 @@
+use reqwest::Client;
+
 pub async fn check_http(target: &str, timeout: std::time::Duration) -> anyhow::Result<bool> {
+    let client = Client::new();
+    let _res = client
+        .get(target)
+        .timeout(timeout)
+        .body(reqwest::Body::default())
+        .send()
+        .await?;
+
     Ok(true)
 }
