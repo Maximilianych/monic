b8a8f276bc07e084387e96360efa894232d7811f
Maximilianych <maxgorochov27@gmail.com>
2025-06-30 00:25:52 +0300

    Возможность остановить задачу мониторинга сервиса через токен

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,10 +1,16 @@
+use tokio_util::sync::CancellationToken;
+
 use crate::{checker::check_http, config::ServiceConfig};
 
-pub async fn start_service_monitor(service: ServiceConfig) {
+pub async fn start_service_monitor(service: ServiceConfig, cancel_token: CancellationToken) {
     let mut interval = tokio::time::interval(service.interval);
 
     loop {
-        interval.tick().await;
+        // Ожидание тика или завершения от токена
+        tokio::select! {
+            _ = interval.tick() => {}
+            _ = cancel_token.cancelled() => break
+        }
 
         match service.service_type.as_str() {
             "http" => {
